c90e8b4820fc11e531408809a1faa9c397c5be07
Feven Samuel <fevensamuelrahel@gmail.com>
2025-09-20 19:01:56 -1100

    Fix argument validation in 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,11 @@
 
-const x = parseInt(process.argv[2]);
+const x = process.argv[2];
 
 if (isNaN(x) || x <= 0) {
   console.log("Missing number of occurrences");
 } else {
-  for (let i = 0; i < x; i++) {
+   const num = parseInt(x);
+  for (let i = 0; i < num; i++) {
     console.log("C is fun");
   }
 }
