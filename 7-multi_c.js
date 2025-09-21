e041848e797557e20c69c7995100238de2737cc7
Feven Samuel <fevensamuelrahel@gmail.com>
2025-09-20 19:06:29 -1100

    Fix: Properly handle negative and non-integer values

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,10 @@
-
 const x = process.argv[2];
 
-if (isNaN(x) || x <= 0) {
+if (isNaN(x) || x <= 0 || !Number.isInteger(Number(x))) {
   console.log("Missing number of occurrences");
 } else {
-   const num = parseInt(x);
+  const num = parseInt(x);
+
   for (let i = 0; i < num; i++) {
     console.log("C is fun");
   }
